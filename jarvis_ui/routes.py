5873e4b14e71d194be1e86fed4f5180882e544d7
Smith Family <smithfamily7@gmail.com>
2025-07-12 16:51:33 -0700

    Fix: Indentation error in jarvis_ui.routes

--- jarvis_ui/routes.py
+++ jarvis_ui/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, Blueprint 
-from . import jarvis_ui 
- 
-@jarvis_ui.route('/') 
-def home(): 
+from flask import render_template, Blueprint
+from . import jarvis_ui
+
+@jarvis_ui.route('/')
+def home():
+    return '<h1>Welcome to Jarvis Dashboard</h1>'
