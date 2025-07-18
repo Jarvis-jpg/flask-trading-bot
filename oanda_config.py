8e7415d657c575ffc21ea2f0071a76c3072c1192
Smith Family <smithfamily7@gmail.com>
2025-07-18 14:46:38 -0700

    Oanda_config

--- oanda_config.py
+++ oanda_config.py
@@ -1,3 +1,3 @@
 OANDA_API_KEY = "e9e9a0366a2b21ebbb03b965301f0182-ea0448d5fc88dd622b361855758d7f3e"
 OANDA_ACCOUNT_ID = "001-001-12623605-001"
-OANDA_URL = "https://api-fxpractice.oanda.com/v3"
+OANDA_API_URL = "https://api-fxtrade.oanda.com/v3"
